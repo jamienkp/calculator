af56add73715368b1025fa5e367af10a53db77e0
Jamie <20918811@student.curtin.edu.au>
2023-09-21 10:30:09 +0400

    Added type hints

--- src/calculator.py
+++ src/calculator.py
@@ -1,43 +1,69 @@
 # calculator/calculations.py
 
-def add(a, b):
+from typing import Union
+
+def add(a: Union[float, int], b: Union[float, int]) -> float:
     """Compute and return the sum of two numbers.
 
+    Examples:
+        >>> add(4.0, 2.0)
+        6.0
+        >>> add(4, 2)
+        6.0
+
     Args:
-        a (float): A number representing the first addend in the addition.
-        b (float): A number representing the second addend in the addition.
+        a: A number representing the first addend in the addition.
+        b: A number representing the second addend in the addition.
 
     Returns:
-        float: A number representing the arithmetic sum of `a` and `b`.
+        A number representing the arithmetic sum of `a` and `b`.
     """
     return float(a + b)
 
-def subtract(a, b):
+def subtract(a: Union[float, int], b: Union[float, int]) -> float:
     """Compute and return the difference of two numbers.
 
+    Examples:
+        >>> subtract(4.0, 2.0)
+        2.0
+        >>> subtract(4, 2)
+        2.0
+
     Args:
-        a (float): A number representing the first number in the subtraction.
-        b (float): A number representing the second number in the subtraction.
+        a: A number representing the first number in the subtraction.
+        b: A number representing the second number in the subtraction.
 
     Returns:
         float: A number representing the arithmetic difference of `a` and `b`.
     """
     return float(a - b)
 
-def multiply(a, b):
+def multiply(a: Union[float, int], b: Union[float, int]) -> float:
     """Compute and return the multiplication of two numbers.
 
+    Examples:
+        >>> multiply(4.0, 2.0)
+        8.0
+        >>> multiply(4, 2)
+        8.0
+
     Args:
-        a (float): A number representing the first number in the multiplication.
-        b (float): A number representing the second number in the multiplication.
+        a: A number representing the first number in the multiplication.
+        b: A number representing the second number in the multiplication.
 
     Returns:
         float: A number representing the arithmetic multiplication of `a` and `b`.
     """
     return float(a * b)
 
-def divide(a, b):
+def divide(a: Union[float, int], b: Union[float, int]) -> float:
     """Compute and return the division of two numbers.
+
+    Examples:
+        >>> divide(4.0, 2.0)
+        2.0
+        >>> divide(4, 2)
+        2.0
 
     Args:
         a (float): A number representing the first number in the division.
